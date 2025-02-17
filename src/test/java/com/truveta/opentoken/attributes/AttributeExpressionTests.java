52c88d3eb7ede76b850fbb498f4dc59a7a6785e7
Matthias Wiselka <matthiasw@truveta.com>
2025-02-17 13:21:05 +0000

    Refactor AttributeExpressionTests to use specific attribute classes and improve test clarity

--- src/test/java/com/truveta/opentoken/attributes/AttributeExpressionTests.java
+++ src/test/java/com/truveta/opentoken/attributes/AttributeExpressionTests.java
@@ -1,6 +1,6 @@
 /**
- * Copyright (c) Truveta. All rights reserved.
- */
+* Copyright (c) Truveta. All rights reserved.
+*/
 
 package com.truveta.opentoken.attributes;
 
@@ -9,13 +9,14 @@ import java.util.UUID;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import com.truveta.opentoken.tokens.BaseTokenDefinition;
+import com.truveta.opentoken.attributes.general.RecordIdAttribute;
+import com.truveta.opentoken.attributes.person.SocialSecurityNumberAttribute;
 
-public class AttributeExpressionTests {
+class AttributeExpressionTests {
 
     @Test
-    public void getEffectiveValue_With_Empty_Expression_Works() {
-        var attribute = new AttributeExpression("RecordId", null);
+    void getEffectiveValue_With_Empty_Expression_Works() {
+        var attribute = new AttributeExpression(RecordIdAttribute.class, null);
         var value = UUID.randomUUID().toString();
         var result = attribute.getEffectiveValue(value);
 
@@ -23,8 +24,8 @@ public class AttributeExpressionTests {
     }
 
     @Test
-    public void getEffectiveValue_With_Uppercase_Expression_Works() {
-        var attribute = new AttributeExpression("RecordId", "U");
+    void getEffectiveValue_With_Uppercase_Expression_Works() {
+        var attribute = new AttributeExpression(RecordIdAttribute.class, "U");
         var value = "abcd";
         var result = attribute.getEffectiveValue(value);
 
@@ -32,18 +33,18 @@ public class AttributeExpressionTests {
     }
 
     @Test
-    public void getEffectiveValue_With_Trim_Expression_Works() {
-        var attribute = new AttributeExpression("RecordId", "T");
-        var value = " abcd  ";
+    void getEffectiveValue_With_Trim_Expression_Works() {
+        var attribute = new AttributeExpression(RecordIdAttribute.class, "T");
+        var value = " abcd ";
         var result = attribute.getEffectiveValue(value);
 
         Assertions.assertEquals("abcd", result);
     }
 
     @Test
-    public void getEffectiveValue_With_Substring_Expression_Works() {
-        var attribute1 = new AttributeExpression("RecordId", "s(0,1)");
-        var attribute2 = new AttributeExpression("RecordId", "S(3,4)");
+    void getEffectiveValue_With_Substring_Expression_Works() {
+        var attribute1 = new AttributeExpression(RecordIdAttribute.class, "s(0,1)");
+        var attribute2 = new AttributeExpression(RecordIdAttribute.class, "S(3,4)");
         var value = "abcd";
         var result1 = attribute1.getEffectiveValue(value);
         var result2 = attribute2.getEffectiveValue(value);
@@ -53,9 +54,9 @@ public class AttributeExpressionTests {
     }
 
     @Test
-    public void getEffectiveValue_With_Substring_OutOfBounds_Works() {
-        var attribute1 = new AttributeExpression("RecordId", "s(0,1)");
-        var attribute2 = new AttributeExpression("RecordId", "S(3,16)");
+    void getEffectiveValue_With_Substring_OutOfBounds_Works() {
+        var attribute1 = new AttributeExpression(RecordIdAttribute.class, "s(0,1)");
+        var attribute2 = new AttributeExpression(RecordIdAttribute.class, "S(3,16)");
         var value = "abcd";
         var result1 = attribute1.getEffectiveValue(value);
         var result2 = attribute2.getEffectiveValue(value);
@@ -65,9 +66,9 @@ public class AttributeExpressionTests {
     }
 
     @Test
-    public void getEffectiveValue_With_Replace_Expression_Works() {
-        var attribute1 = new AttributeExpression("RecordId", "R('/','-')");
-        var attribute2 = new AttributeExpression("RecordId", "r('9999','')");
+    void getEffectiveValue_With_Replace_Expression_Works() {
+        var attribute1 = new AttributeExpression(RecordIdAttribute.class, "R('/','-')");
+        var attribute2 = new AttributeExpression(RecordIdAttribute.class, "r('9999','')");
         var value = "99/99/9999";
         var result1 = attribute1.getEffectiveValue(value);
         var result2 = attribute2.getEffectiveValue(value);
@@ -77,8 +78,8 @@ public class AttributeExpressionTests {
     }
 
     @Test
-    public void getEffectiveValue_With_Match_Expression_Works() {
-        var attribute1 = new AttributeExpression(BaseTokenDefinition.SOCIAL_SECURITY_NUMBER, "M(\\d+)");
+    void getEffectiveValue_With_Match_Expression_Works() {
+        var attribute1 = new AttributeExpression(SocialSecurityNumberAttribute.class, "M(\\d+)");
         var value1 = "123-45-6789";
         var result1 = attribute1.getEffectiveValue(value1);
 
@@ -86,8 +87,8 @@ public class AttributeExpressionTests {
     }
 
     @Test
-    public void getEffectiveValue_With_Multiple_Expressions_Works() {
-        var attribute1 = new AttributeExpression("RecordId", "U|T|R('.','')|S(5,7)");
+    void getEffectiveValue_With_Multiple_Expressions_Works() {
+        var attribute1 = new AttributeExpression(RecordIdAttribute.class, "U|T|R('.','')|S(5,7)");
 
         var value = "1234 56th Ave.";
         var result1 = attribute1.getEffectiveValue(value);
@@ -96,17 +97,17 @@ public class AttributeExpressionTests {
     }
 
     @Test
-    public void invalid_Expression_Throws() {
+    void invalid_Expression_Throws() {
         var value = "whatever";
 
-        var attribute = new AttributeExpression("RecordId", "R(5,9)");
+        var attribute = new AttributeExpression(RecordIdAttribute.class, "R(5,9)");
         try {
             attribute.getEffectiveValue(value);
         } catch (Exception ex) {
             Assertions.assertTrue(ex instanceof IllegalArgumentException);
         }
 
-        attribute = new AttributeExpression("RecordId", "S('d',9)");
+        attribute = new AttributeExpression(RecordIdAttribute.class, "S('d',9)");
         try {
             attribute.getEffectiveValue(value);
         } catch (Exception ex) {
