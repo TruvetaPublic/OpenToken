730a1d71978bb0a81759c0915dfe364de0cac7a9
Matthias Wiselka <matthiasw@truveta.com>
2025-07-08 18:14:13 +0000

    Update Metadata.java version to match current pom.xml version

--- src/main/java/com/truveta/opentoken/Metadata.java
+++ src/main/java/com/truveta/opentoken/Metadata.java
@@ -19,7 +19,7 @@ public class Metadata {
     public static final String METADATA_FILE_EXTENSION = ".metadata.json";
     public static final String SYSTEM_JAVA_VERSION = System.getProperty("java.version");
 
-    public static final String DEFAULT_VERSION = "1.7.0";
+    public static final String DEFAULT_VERSION = "1.8.2";
 
     // Output format values
     public static final String OUTPUT_FORMAT_JSON = "JSON";
