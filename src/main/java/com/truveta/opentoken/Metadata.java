05e87699a10ad7c57f19976d27a231ec9690524b
Matthias Wiselka <matthiasw@truveta.com>
2025-07-08 18:14:34 +0000

    Bump version from 1.8.2 to 1.9.0 (minor version bump)

--- src/main/java/com/truveta/opentoken/Metadata.java
+++ src/main/java/com/truveta/opentoken/Metadata.java
@@ -19,7 +19,7 @@ public class Metadata {
     public static final String METADATA_FILE_EXTENSION = ".metadata.json";
     public static final String SYSTEM_JAVA_VERSION = System.getProperty("java.version");
 
-    public static final String DEFAULT_VERSION = "1.8.2";
+    public static final String DEFAULT_VERSION = "1.9.0";
 
     // Output format values
     public static final String OUTPUT_FORMAT_JSON = "JSON";
