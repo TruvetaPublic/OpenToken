e6db56bd100067ef8a7c22e1507b920a2a383cd0
Serhiy Leonchyk <serhiyl@uw.edu>
2025-07-30 14:37:33 -0700

    minor error

--- lib/python/src/main/opentoken/metadata.py
+++ lib/python/src/main/opentoken/metadata.py
@@ -25,7 +25,7 @@ class Metadata:
     METADATA_FILE_EXTENSION = ".metadata.json"
     SYSTEM_PYTHON_VERSION = f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
     
-    DEFAULT_VERSION = "1.9.1"
+    DEFAULT_VERSION = "1.9.3"
     
     # Output format values
     OUTPUT_FORMAT_JSON = "JSON"
